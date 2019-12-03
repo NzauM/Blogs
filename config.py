1e483daaede682ffa7be4ff7a1848c8b77ef112d
NzauM <mercywaenu16@gmail.com>
2019-12-03 12:11:08 +0300

    Deployment

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
     '''
     Configuration child class for production
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
     '''
