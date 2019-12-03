c6282b8d1910d3210876657fbfe2bda0c50ed690
NzauM <mercywaenu16@gmail.com>
2019-12-03 12:20:48 +0300

    Fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -19,12 +19,11 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
-    app = Flask(__name__,instance_relative_config=True)
+    app = Flask(__name__)
 
 
 
-    app.config.from_object(DevConfig)
-    app.config.from_pyfile('config.py')
+    
 
     bootstrap.init_app(app)
     db.init_app(app)
